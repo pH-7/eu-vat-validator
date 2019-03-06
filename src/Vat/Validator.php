43921e5725bfad02ee27d898025a6aaf86d99c63
Pierre-Henry Soria <hi@ph7.me>
2019-03-06 20:40:18 +1300

    Bump Year (copyright header)

--- src/Vat/Validator.php
+++ src/Vat/Validator.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author         Pierre-Henry Soria <pierrehenrysoria@gmail.com>
- * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright      (c) 2017-2019, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
  */
 
--- src/autoloader.php
+++ src/autoloader.php
@@ -3,7 +3,7 @@
  * Manual Autoloader File (if composer isn't installed)
  *
  * @author         Pierre-Henry Soria <pierrehenrysoria@gmail.com>
- * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright      (c) 2017-2019, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
  */
 
